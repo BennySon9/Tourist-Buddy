a160c3dba93537a704717071aeaf6177f565516d
Scott Ashley <burnbright987@gmail.com>
2022-05-11 19:28:20 -0400

    added api url

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,7 @@
 var amadeusApiKey = fYFFYtB8IOl7sJtJssJ2AABnwUvetWiG;
+
+
+function getWeatherData(data) {
+    var openWeatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=imperial&exclude=alerts,hourly,minutely,current&appid=6e946ccd39b3086fbf89b034cf7a8b3e"
+    fetch()
+}
