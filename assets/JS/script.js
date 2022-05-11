b0c0ce42869df442e4d26b077c42d0a674b9447e
MIshmail <michaely.ishmail@gmail.com>
2022-05-11 19:28:11 -0400

    basic js

---
+++ assets/JS/script.js
@@ -0,0 +1,9 @@
+var destinatinEL = document.getElementById('destination')
+var departureDateEL = document.getElementById('departure-date')
+var returnnDateEL = document.getElementById('return-date')
+var searchEL = document.getElementById('search')
+
+
+document.getElementById('search').onclick = function(){
+    console.log(destinatinEL, departureDateEL, returnnDateEL)
+}
