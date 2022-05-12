4298401c35905e2e709105a795628a6ab43838c3
Scott Ashley <burnbright987@gmail.com>
2022-05-11 20:48:05 -0400

    working on fetch function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,11 @@
-var amadeusApiKey = fYFFYtB8IOl7sJtJssJ2AABnwUvetWiG;
+
 
 
 function getWeatherData(data) {
-    var openWeatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=imperial&exclude=alerts,hourly,minutely,current&appid=6e946ccd39b3086fbf89b034cf7a8b3e"
-    fetch()
+    var openWeatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=alerts,hourly,minutely,current&appid=6e946ccd39b3086fbf89b034cf7a8b3e"
+    fetch(openWeatherApiUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        console.log(response);
 }
