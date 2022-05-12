dc3a602708a0da67789d91bf7c64e95724f70685
MIshmail <michaely.ishmail@gmail.com>
2022-05-11 20:28:33 -0400

    added local storage

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,9 +1,23 @@
-var destinatinEL = document.getElementById('destination')
+var destinationEL = document.getElementById('destination')
 var departureDateEL = document.getElementById('departure-date')
-var returnnDateEL = document.getElementById('return-date')
+var returnDateEL = document.getElementById('return-date')
 var searchEL = document.getElementById('search')
 
 
 document.getElementById('search').onclick = function(){
-    console.log(destinatinEL, departureDateEL, returnnDateEL)
+
+    let destination = JSON.stringify(destinationEL.value)
+    localStorage.setItem("destination", destination)
+    JSON.parse(localStorage.getItem(destinationEL))
+
+    let departureDate = JSON.stringify(departureDateEL.value)
+    localStorage.setItem("departure", JSON.stringify(departureDate))
+    JSON.parse(localStorage.getItem(departureDateEL))
+
+    let returnDate = JSON.stringify(returnDateEL.value)
+    localStorage.setItem("return", JSON.stringify(returnDate))
+    JSON.parse(localStorage.getItem(returnDateEL))
+
+    console.log(localStorage)
+    
 }
