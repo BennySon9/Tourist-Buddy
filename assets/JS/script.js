66714ebfa8ebab02042a590d6525cf653933584b
MIshmail <michaely.ishmail@gmail.com>
2022-05-14 19:23:06 -0400

    added full event functionality

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -26,6 +26,36 @@ document.getElementById('search').onclick = function(){
         async:true,
         dataType: "json",
         success: function(json) {
+            var event0Name = json._embedded.events[0].name
+            $('.event-0-p').append(event0Name)
+
+            var event0Img = json._embedded.events[0].images[0].url
+            $(".event-icon-0").attr("src", event0Img)
+    
+            var event1Name = json._embedded.events[1].name
+            $('.event-1-p').append(event1Name)
+
+            var event1Img = json._embedded.events[1].images[0].url
+            $(".event-icon-1").attr("src", event1Img)
+
+            var event2Name = json._embedded.events[2].name
+            $('.event-2-p').append(event2Name)
+
+            var event2Img = json._embedded.events[2].images[0].url
+            $(".event-icon-2").attr("src", event2Img)
+
+            var event3Name = json._embedded.events[3].name
+            $('.event-3-p').append(event3Name)
+
+            var event3Img = json._embedded.events[3].images[0].url
+            $(".event-icon-3").attr("src", event3Img)
+
+            var event4Name = json._embedded.events[4].name
+            $('.event-4-p').append(event4Name)
+
+            var event4Img = json._embedded.events[4].images[0].url
+            $(".event-icon-4").attr("src", event4Img)
+                    
                     console.log(json)
                     // Parse the response.
                     // Do other things.
