47c5af3a2fbc88316ddb077388dc298ebb99597c
MIshmail <michaely.ishmail@gmail.com>
2022-05-14 16:19:35 -0400

    added ticketmaster api

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -21,3 +21,18 @@ document.getElementById('search').onclick = function(){
     console.log(localStorage)
     
 }
+
+$.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=J5c4RhQvGVlW2JcXGoyA62yfl9i1K8iA",
+    async:true,
+    dataType: "json",
+    success: function(json) {
+                console.log(json);
+                // Parse the response.
+                // Do other things.
+             },
+    error: function(xhr, status, err) {
+                // This time, we do not end up here!
+             }
+  });
