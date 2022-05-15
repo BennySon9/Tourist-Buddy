be45eaa66eb59eeda77f729bc761f755ef1b59dd
Scott Ashley <burnbright987@gmail.com>
2022-05-15 19:50:05 -0400

    This is what I have so far. It adds a forecast for the next 5 days

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,6 +3,8 @@ var departureDateEL = document.getElementById('departure-date')
 var returnDateEL = document.getElementById('return-date')
 var searchEL = document.getElementById('search')
 
+var destinationWeatherEl = document.getElementById("destinationWeather")
+var openWeatherApiKey = "6e946ccd39b3086fbf89b034cf7a8b3e"
 
 document.getElementById('search').onclick = function(){
 
@@ -20,13 +22,52 @@ document.getElementById('search').onclick = function(){
 
     console.log(localStorage)
     
+    getWeather();
 }
 
-function getWeatherData(data) {
-    var openWeatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=alerts,hourly,minutely,current&appid=6e946ccd39b3086fbf89b034cf7a8b3e"
-    fetch(openWeatherApiUrl)
-        .then(function(response) {
-            return response.json();
-        })
-        console.log(response);
+function getWeather() {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + destinationEL.value + "&appid=" + openWeatherApiKey;
+
+    fetch(requestUrl).then(function(response) {
+        response.json().then(function(data) {
+        var cityId = data.id;
+        var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityId + "&appid=" + openWeatherApiKey;
+
+        fetch(forecastUrl).then(function(response) {
+            response.json().then(function(data) {
+                var forecastEls = document.querySelectorAll(".forecast");
+                for (i = 0; i < forecastEls.length; i++) {
+                    forecastEls[i].innerHTML = "";
+                    forecastEls[i].classList.add("border-2", "border-black");
+                    var forecastIndex = i * 8 + 4;
+                    var forecastDate = new Date(data.list[forecastIndex].dt * 1000);
+                    var forecastDay = forecastDate.getDate();
+                    var forecastMonth = forecastDate.getMonth() + 1;
+                    var forecastYear = forecastDate.getFullYear();
+                    var forecastDateEl = document.createElement("p");
+                    forecastDateEl.setAttribute("class", "forecast-date");
+                    forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+                    forecastEls[i].append(forecastDateEl);
+
+                    var forecastWeatherEl = document.createElement("img");
+                    forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[forecastIndex].weather[0].icon + "@2x.png");
+                    forecastWeatherEl.setAttribute("alt", data.list[forecastIndex].weather[0].description);
+                    forecastEls[i].append(forecastWeatherEl);
+
+                    var forecastTempEl = document.createElement("p");
+                    forecastTempEl.innerHTML = "Temp: " + k2f(data.list[forecastIndex].main.temp) + "°";
+                    forecastEls[i].append(forecastTempEl);
+
+                    var forecastHumidityEl = document.createElement("p");
+                    forecastHumidityEl.innerHTML = "Humidity: " + data.list[forecastIndex].main.humidity + "%";
+                    forecastEls[i].append(forecastHumidityEl);
+                }
+            });
+            })
+        });
+    })
+}
+
+function k2f(K) {
+    return Math.floor((K - 273.15) * 1.8 + 32);
 }
