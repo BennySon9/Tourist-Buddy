4b43e5e5d80e37ad52c1cc5ef39c9fc75f0ce9b4
Scott Ashley <burnbright987@gmail.com>
2022-05-16 11:19:16 -0400

    changed forecast url/ forecast now shows 7 days

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,7 +4,6 @@ var returnDateEL = document.getElementById('return-date')
 var searchEL = document.getElementById('search')
 
 var destinationWeatherEl = document.getElementById("destinationWeather")
-var openWeatherApiKey = "6e946ccd39b3086fbf89b034cf7a8b3e"
 
 document.getElementById('search').onclick = function(){
 
@@ -26,12 +25,11 @@ document.getElementById('search').onclick = function(){
 }
 
 function getWeather() {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + destinationEL.value + "&appid=" + openWeatherApiKey;
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + destinationEL.value + "&appid=6e946ccd39b3086fbf89b034cf7a8b3e";
 
     fetch(requestUrl).then(function(response) {
         response.json().then(function(data) {
-        var cityId = data.id;
-        var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityId + "&appid=" + openWeatherApiKey;
+        var forecastUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude=current,hourly,minutely,alerts&appid=6e946ccd39b3086fbf89b034cf7a8b3e";
 
         fetch(forecastUrl).then(function(response) {
             response.json().then(function(data) {
@@ -39,8 +37,8 @@ function getWeather() {
                 for (i = 0; i < forecastEls.length; i++) {
                     forecastEls[i].innerHTML = "";
                     forecastEls[i].classList.add("border-2", "border-black");
-                    var forecastIndex = i * 8 + 4;
-                    var forecastDate = new Date(data.list[forecastIndex].dt * 1000);
+
+                    var forecastDate = new Date(data.daily[i].dt * 1000);
                     var forecastDay = forecastDate.getDate();
                     var forecastMonth = forecastDate.getMonth() + 1;
                     var forecastYear = forecastDate.getFullYear();
@@ -50,16 +48,16 @@ function getWeather() {
                     forecastEls[i].append(forecastDateEl);
 
                     var forecastWeatherEl = document.createElement("img");
-                    forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[forecastIndex].weather[0].icon + "@2x.png");
-                    forecastWeatherEl.setAttribute("alt", data.list[forecastIndex].weather[0].description);
+                    forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png");
+                    forecastWeatherEl.setAttribute("alt", data.daily[i].weather[0].description);
                     forecastEls[i].append(forecastWeatherEl);
 
                     var forecastTempEl = document.createElement("p");
-                    forecastTempEl.innerHTML = "Temp: " + k2f(data.list[forecastIndex].main.temp) + "°";
+                    forecastTempEl.innerHTML = "Temp: " + k2f(data.daily[i].temp.day) + "°";
                     forecastEls[i].append(forecastTempEl);
 
                     var forecastHumidityEl = document.createElement("p");
-                    forecastHumidityEl.innerHTML = "Humidity: " + data.list[forecastIndex].main.humidity + "%";
+                    forecastHumidityEl.innerHTML = "Humidity: " + data.daily[i].humidity + "%";
                     forecastEls[i].append(forecastHumidityEl);
                 }
             });
