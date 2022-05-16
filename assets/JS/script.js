329a0fdff09e603c3b03caf45a97a6d6d58eb81d
MIshmail <michaely.ishmail@gmail.com>
2022-05-16 19:34:06 -0400

    fixed java errors

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -29,11 +29,45 @@ document.getElementById('search').onclick = function(){
         success: function(json) {
 
 
-                    var event0Name = json._embedded.events[0].name
-                    $('.event-0-p').empty().append(event0Name)
+            var event0Name = json._embedded.events[0].name
+            $('.event-0-p').empty().append(event0Name)
 
-                    var event0Img = json._embedded.events[0].images[1].url
-                    $(".event-icon-0").attr("src", event0Img)
+            var event0Img = json._embedded.events[0].images[1].url
+            $(".event-icon-0").attr("src", event0Img)
+
+            var event1Name = json._embedded.events[1].name
+            $('.event-1-p').empty().append(event1Name)
+
+            var event1Img = json._embedded.events[1].images[1].url
+            $(".event-icon-1").attr("src", event1Img)
+
+            var event2Name = json._embedded.events[2].name
+            $('.event-2-p').empty().append(event2Name)
+
+            var event2Img = json._embedded.events[2].images[1].url
+            $(".event-icon-2").attr("src", event2Img)
+
+            var event3Name = json._embedded.events[3].name
+            $('.event-3-p').empty().append(event3Name)
+
+            var event3Img = json._embedded.events[3].images[1].url
+            $(".event-icon-3").attr("src", event3Img)
+
+            var event4Name = json._embedded.events[4].name
+            $('.event-4-p').empty().append(event4Name)
+
+            var event4Img = json._embedded.events[4].images[1].url
+            $(".event-icon-4").attr("src", event4Img)
+        
+            console.log(json)
+            
+            // Parse the response.
+            // Do other things.
+         },
+error: function(xhr, status, err) {
+            // This time, we do not end up here!
+         }
+});
     
     getWeather();
 }
